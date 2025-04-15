d34733a993dff707384ea228cf614a52bac72649
Kyle Murphy <murphy.kyle.r@gmail.com>
2025-04-15 06:27:40 -0400

    Update io.py
    Added function to load daily fism2  flare save files. This is used to append data to the feature dataframe as new data become available.
    
    Fixed an issue where OMNI data wouldn't load the last year of data.
    
    Fixed issue that OMNI data wouldn't load if only a single file was passed.

--- mltdm/io.py
+++ mltdm/io.py
@@ -7,6 +7,10 @@ import pandas as pd
 import numpy as np
 import netCDF4 as nc
 
+from scipy.io import readsav
+from urllib.parse import urljoin
+from urllib.request import urlretrieve
+
 import mltdm
 
 
@@ -62,12 +66,58 @@ def fism_flare(http_path: str=mltdm.c_dat['fism_flare'],
         df=df[rcols]
     
     return df, desc
-    
-    pass
 
-def fism_flare_day():
+
+def fism_flare_day(http_path: str=mltdm.c_dat['fism_flare'],
+                   data_path: str=mltdm.c_dat['data_dir'],
+                   sdate: str='2024-01-01', edate: str='2024-01-02',
+                   rcols: list=None):
     # todo read in daily files to append to larger data file
-    pass
+    pre = 'FISM_bands_'
+    suf = '_v02_01.sav'
+
+    d_ser = pd.date_range(start=sdate, end=edate, freq='1D')
+    fn_http = [urljoin(http_path,
+                       f'{x.year}/{pre}{x.year}{x.day_of_year:03}{suf}')
+               for x in d_ser]
+    fn_local = [os.path.join(data_path,
+                       f'{x.year}',
+                       f'{pre}{x.year}{x.day_of_year:03}{suf}')
+                for x in d_ser]
+    
+    df_r = pd.DataFrame()
+    for fn_l, fn_w in zip(fn_local, fn_http):
+        df_i = pd.DataFrame()
+        
+        if os.path.exists(fn_l):
+            dat = readsav(fn_l)
+        else:
+            fn_t = os.path.join(data_path,'temp.sav')
+            urlretrieve(fn_w, fn_t)
+            dat = readsav(fn_t)
+        
+        wvlen = dat['wavelength']
+        ssi = dat['ssi']
+        
+        df_i['DateTime'] = pd.to_datetime(dat['jd'], unit='D',origin='julian')
+        for i in np.arange(0,wvlen.shape[0]):
+            df_i[f'{round(wvlen[i]*1000)}_{i:02}'] = ssi[:,i]
+        
+        if df_r.empty:
+            df_r = df_i.copy()
+        else:
+            df_r = pd.concat([df_r,df_i],ignore_index=True)
+        
+    if os.path.exists(os.path.join(data_path,'temp.sav')):
+        os.remove(os.path.join(data_path,'temp.sav'))
+    
+    df_r['DateTime'] = df_r['DateTime'].dt.round('1min')
+    # TODO check if values are actually in columns
+    if rcols:
+        df_r=df_r[rcols]
+       
+    return df_r
+    
 
 def omni(http_path: str=mltdm.c_dat['omni'],
          sdate: str='2003', edate: str='2004',
@@ -76,7 +126,12 @@ def omni(http_path: str=mltdm.c_dat['omni'],
     sdate = pd.to_datetime(sdate)
     edate = pd.to_datetime(edate)
     #
-    d_ser = pd.date_range(start=sdate, end=edate, freq='12MS')
+    d_ser = pd.date_range(start=f'{sdate.year}', 
+                          end=f'{edate.year}', freq='12MS')
+    if d_ser.empty:
+        d_ser = pd.date_range(start=f'{sdate.year}', 
+                              end=f'{sdate.year+1}', freq='12MS')
+        
     fn = [os.path.join(http_path,f'omni_5min{x.year}.asc') for x in d_ser]
     
     # TODO maybe at this to a _utils.py and import so it's less messy?
@@ -101,11 +156,16 @@ def omni(http_path: str=mltdm.c_dat['omni'],
     dates = ['Year','DOY','Hour','Minute']
     
     # read in all the data
-    om_dat = pd.concat((pd.read_csv(f,sep='\s+', engine='python', 
+    if len(fn) > 1:
+        om_dat = pd.concat((pd.read_csv(f,sep='\s+', engine='python', 
                                     names=list(dcols.keys()),header=None, 
                                     on_bad_lines='skip') 
-                        for f in fn), 
-                       ignore_index=True)
+                                    for f in fn), 
+                           ignore_index=True)
+    else:
+        om_dat = pd.read_csv(fn[0],sep='\s+', engine='python', 
+                                    names=list(dcols.keys()),header=None, 
+                                    on_bad_lines='skip') 
     
     # replace missing or bad data with NaN's
     for k, kval in dcols.items():
@@ -125,5 +185,3 @@ def omni(http_path: str=mltdm.c_dat['omni'],
         om_dat = om_dat[rcols]
     
     return om_dat
-
-
