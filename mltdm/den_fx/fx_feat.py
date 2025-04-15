3e1176723fa4ebcb58f326a01e9b7c6450da2052
Kyle Murphy <murphy.kyle.r@gmail.com>
2025-04-15 06:31:06 -0400

    Update fx_feat.py
    Fixed an issue where the FISM2 netcdf file didn't have good data after 2022. Dropped data after this.
    
    Added append_feat which will append data to the feature array up to an end date if the data wasn't there.
    
    Added append_feat to load_feat.
    If the difference between the end of the loaded feature data and the end date requested is larger then a day, then append is called in order to get the rest of the required feature data and append it to the feature dataframe.

--- mltdm/den_fx/fx_feat.py
+++ mltdm/den_fx/fx_feat.py
@@ -20,12 +20,14 @@ def create_feat():
     
     fism, _ = io.fism_flare(rcols=fism_cols)
     
+    # the netcdf file has large gaps after 2022-01-01
+    # hard code edate so only continous data is loaded
     sdate = fism['DateTime'].min()+pd.offsets.YearBegin(-1)
-    edate = fism['DateTime'].max()+pd.offsets.YearBegin(0)
+    edate = pd.to_datetime('2022-01-01')
     
     omni = io.omni(sdate=sdate,edate=edate, rcols=omni_cols)
     omni = omni[(omni['DateTime'] >= fism['DateTime'].min()) &
-                (omni['DateTime'] <= fism['DateTime'].max())]
+                (omni['DateTime'] < edate)]
     
     
     omni = omni.set_index('DateTime')
@@ -42,8 +44,44 @@ def create_feat():
                     complevel=2,format='table', data_columns=['DateTime'])
      
     
-def append_feat():
-    pass
+def append_feat(edate: str=None):
+    
+    feat_f = os.path.join(mltdm.c_dat['data_dir'],'fx_den_feat.hdf')
+    feat_dat = pd.read_hdf(feat_f)
+    
+    tol = pd.Timedelta('2.5 minute')
+    fism_cols = ['1300_02', '43000_09', '85550_13', '94400_18', 'DateTime']
+    omni_cols = ['SYM_H index', 'AE', 'DateTime']
+    
+    sdate = feat_dat['DateTime'].max()
+    sdate = f'{sdate.year:04}-{sdate.month:02}-{sdate.day:02}'
+    
+    # read in the new data
+    fism = io.fism_flare_day(sdate=sdate,edate=edate, rcols=fism_cols)
+    omni = io.omni(sdate=sdate,edate=edate, rcols=omni_cols)
+    omni = omni[(omni['DateTime'] >= fism['DateTime'].min()) &
+                (omni['DateTime'] <= fism['DateTime'].max())]
+    
+    omni = omni.set_index('DateTime')
+    fism = fism.set_index('DateTime')
+    
+    # merge the omni and FISM data
+    feat_app = pd.merge_asof(left=fism,right=omni,
+                             right_index=True,left_index=True,
+                             direction='nearest',tolerance=tol)
+    
+    feat_app = feat_app.reset_index()
+    
+    # append the new data to original data
+    feat_new = pd.concat([feat_dat,feat_app],ignore_index=True)
+    feat_new = feat_new.drop_duplicates(subset='DateTime')
+    feat_new = feat_new.reset_index(drop=True)
+    
+    out_f = os.path.join(mltdm.c_dat['data_dir'],'fx_den_feat.hdf')
+    feat_new.to_hdf(out_f,key='fx_den_feat',
+                    complevel=2,format='table', data_columns=['DateTime'])
+    
+    return feat_new
 
 
 def load_feat(sdate: str=None, edate: str=None):
@@ -73,9 +111,21 @@ def load_feat(sdate: str=None, edate: str=None):
     
     feat_dat = pd.read_hdf(feat_f, where=where)
     
+    if not feat_dat.empty:
+        dt = pd.to_datetime(edate)-feat_dat['DateTime'].max()
+        dt = dt.total_seconds()
+    else:
+        dt = -1
+         
+    if feat_dat.empty or dt > 86399:
+        feat_dat = append_feat(edate=edate)
+        gd = (feat_dat['DateTime']>=sdate) & (feat_dat['DateTime']<=edate)
+        feat_dat = feat_dat[gd]
+    
     # log the columns for predictions
     for i in log_cols:
         feat_dat[i] = np.log10(feat_dat[i])
         
     return feat_dat
+
 
