daa3a477511640be8bc19451a4d5bf7bc5fc846a
Kyle Murphy <murphy.kyle.r@gmail.com>
2025-06-04 11:41:17 -0400

    downloaded url function
    Added download url function with status bar.

--- mltdm/io.py
+++ mltdm/io.py
@@ -2,6 +2,11 @@
 
 import os
 import requests
+import pathlib
+import functools
+import shutil
+
+from tqdm.auto import tqdm
 
 import pandas as pd
 import numpy as np
@@ -11,12 +16,28 @@ from scipy.io import readsav
 from urllib.parse import urljoin
 from urllib.request import urlretrieve
 
+
 import mltdm
 
 
 # TODO I think this should be it's own submodule
 # TODO it could get quite big as we use more datasets
 
+def dl_file(url, filename):
+    r = requests.get(url, stream=True, allow_redirects=True)
+    if r.status_code != 200:
+        r.raise_for_status()  # Will only raise for 4xx codes, so...
+        raise RuntimeError(f"Request to {url} returned status code {r.status_code}")
+    file_size = int(r.headers.get('Content-Length', 0))
+    
+    path = pathlib.Path(filename).expanduser().resolve()
+    path.parent.mkdir(parents=True, exist_ok=True)
+    
+    desc = "(Unknown total file size)" if file_size == 0 else ""
+    r.raw.read = functools.partial(r.raw.read, decode_content=True)  # Decompress if needed
+    with tqdm.wrapattr(r.raw, "read", total=file_size, desc=desc) as r_raw:
+        with path.open("wb") as f:
+            shutil.copyfileobj(r_raw, f)
 
 
 def fism_flare(http_path: str=mltdm.c_dat['fism_flare'],
